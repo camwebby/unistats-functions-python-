78d870f101a70ed9f6191530dfe95c653ca5e545
camwebby <camwebbyhalsall3@gmail.com>
2018-03-15 00:47:22 +0000

    Added subject data

--- csvReader.py
+++ csvReader.py
@@ -1,23 +1,53 @@
 import csv
 
-econ = []
-psychology = []
+econPure = []
+econBreadth = []
+
+psychologyPure = []
+psychologyBreadth = []
+
 engineering = []
 mechanical = []
+civilEng = []
+electricalEng = []
+maths = []
+english = []
+history = []
+law = []
+finance = []
 
 
-with open("data.csv","r") as dataFile:
+header = "Institution, Course, Type, Study Mode, Foundation Year(s), Sandwich Year(s), Total years, Salary (6 months after), Median Tariff"
+
+with open("data2.csv","r") as dataFile:
     read = csv.reader(dataFile)
     for count,row in enumerate(read):
         if count % 2 == 0:
+            if "economics" == row[1].lower():
+                econPure.append(row)
             if "economics" in row[1].lower():
-                econ.append(row)
-            elif "psychology" in row[1].lower():
-                psychology.append(row)
+                econBreadth.append(row)
+                
+            if "psychology" == row[1].lower():
+                psychologyPure.append(row)
+            if "psychology" in row[1].lower():
+                psychologyBreadth.append(row)
+                
             if "engineering" in row[1].lower():
                 engineering.append(row)
             if "mechanical  engineering" in row[1].lower():
                 mechanical.append(row)
 
-                
-print(mechanical)
+
+def writeToCSV(csvFile, subject):
+    with open(csvFile,"w") as file:
+        wr = csv.writer(file)
+        wr.writerow([header])
+        for x in subject:
+            wr.writerow(x)
+
+writeToCSV("psychPure.csv", psychologyPure)
+writeToCSV("psychBreadth.csv", psychologyBreadth)
+
+           
+
